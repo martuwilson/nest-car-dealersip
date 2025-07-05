212601d5616471d0c2ab0649c6f77f6600e39520
martuwilson <williner.dev@gmail.com>
2025-07-05 18:12:11 -0300

    get() en cars

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+    @Get()
+    getAllCars() {
+        return [
+            'Toyota',
+            'Honda',
+            'Ford',
+            'Chevrolet',
+            'BMW',
+            'Mercedes',
+        ];
+    }
+
+}
