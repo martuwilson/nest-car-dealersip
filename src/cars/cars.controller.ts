6cd45779976e907dcd4acc2fe0fd4e2dccdd96b0
martuwilson <williner.dev@gmail.com>
2025-07-06 19:34:13 -0300

    getCarById added

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,18 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = [
+        'Toyota',
+        'Honda',
+        'Ford',
+        'Chevrolet',
+        'BMW',
+        'Mercedes',
+    ];
+
     @Get()
     getAllCars() {
-        return [
-            'Toyota',
-            'Honda',
-            'Ford',
-            'Chevrolet',
-            'BMW',
-            'Mercedes',
-        ];
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById(@Param('id') id: string) {
+        const index = Number(id);
+        if (isNaN(index) || index < 0 || index >= this.cars.length) {
+            return 'Car not found';
+        }
+        return this.cars[index];
     }
 
 }
