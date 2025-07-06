254f20ea42892e67926d66d0acf0f26bf81bbc8e
martuwilson <williner.dev@gmail.com>
2025-07-06 20:37:48 -0300

    feat: agregar exception filter para el 404

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
@@ -17,7 +17,11 @@ export class CarsService {
     }
 
     findById(id: number) {
-        return this.cars.find(car => car.id === id);
+        const car = this.cars.find(car => car.id === id);
+
+        if (!car) throw new NotFoundException(`Car with ID ${id} not found`);
+        
+        return car;
     }
 
 }
